9257dd369eeb043880f06b483164809816ea5304
Adrian Cuadron <76923232+AdrianCuadron@users.noreply.github.com>
2025-11-24 10:21:42 +0100

    Add files via upload

---
+++ parleus/add_id_docs.py
@@ -0,0 +1,41 @@
+import argparse
+import json
+
+# Función para procesar múltiples archivos
+def process_multiple_json(lang: str, root_path: str):
+    current_id = 1
+
+    output_paths = []
+    
+    for i in range(10, 13):
+        output_path = f"{root_path}/legislatura_{i}/parlamentu_{i}_{lang}_final.jsonl"
+        output_paths.append(output_path)
+        with open(output_path, 'w', encoding='utf-8') as out:
+            input_path = f"{root_path}/legislatura_{i}/parlamentu_{i}_{lang}.json"
+            with open(input_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                for item in data:
+                    item["unique_id"] = f"parl_{lang}_{current_id}"
+                    out.write(json.dumps(item, ensure_ascii=False) + "\n")
+                    current_id += 1
+
+    output_file = f"{root_path}/parleus_{lang}.jsonl"
+
+    # leer los 3 jsonl y juntarlos en uno
+    with open(output_file, "w", encoding='utf-8') as outfile:
+        for input_file in output_paths:
+            with open(input_file, "r", encoding='utf-8') as infile:
+                for line in infile:
+                    # cargar el json
+                    json_line = json.loads(line)
+                    # escribir en el nuevo fichero
+                    outfile.write(json.dumps(json_line, ensure_ascii=False) + "\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Add unique IDs to documents")
+    parser.add_argument("--root_path", type=str, help="Root path to add IDs for")
+    args = parser.parse_args()
+    # Procesar los tres archivos por idioma
+    process_multiple_json("es", args.root_path)
+    process_multiple_json("eu", args.root_path)
---
+++ parleus/parse_pdf_txt.py
@@ -0,0 +1,121 @@
+import argparse
+import json
+import time
+import pdfplumber
+from lxml import etree
+import requests
+
+
+def download_pdf(url, max_retries=3, delay=2):
+    nombre_archivo = "doc_temp.pdf"
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/pdf",
+    }
+    time.sleep(2)  # Espera inicial antes de la primera descarga
+    for attempt in range(1, max_retries + 1):
+        try:
+            response = requests.get(url, headers=headers, timeout=10)
+            content_type = response.headers.get("Content-Type", "")
+            
+            if response.status_code == 200 and "application/pdf" in content_type:
+                with open(nombre_archivo, "wb") as f:
+                    f.write(response.content)
+                return True
+            else:
+                print(f"Intento {attempt}: No es un PDF válido (Content-Type: {content_type}), {response.content}")
+        except Exception as e:
+            print(f"Intento {attempt}: Error al descargar el PDF -> {e}")
+
+        time.sleep(delay)  # Espera antes de reintentar
+
+    print("No se pudo descargar un PDF válido tras varios intentos.")
+    return False
+
+def extract_columns(pdf_path, fecha, encabezado_alto=50, pie_alto=50):
+    with pdfplumber.open(pdf_path) as pdf:
+        text_col1, text_col2 = [], []
+        primera_pagina = True
+        for page in pdf.pages:
+            # Divide la página en dos mitades y elimina el encabezado y pie
+            width = page.width
+            height = page.height
+            
+            if primera_pagina:
+                primera_pagina = False
+                text = page.extract_text()
+                if "Bilkuraren hitzez hitzeko transkripzioaren behin-behineko argitalpena" in text or "sustituida en su momento por la publicación definitiva" in text:
+                    print("No es el behin-betiko: ", fecha)
+                    return None
+            
+            # Ajusta las coordenadas para excluir el encabezado y el pie de página
+            left_bbox = (0, encabezado_alto, width / 2, height - pie_alto)
+            right_bbox = (width / 2, encabezado_alto, width, height - pie_alto)
+            
+            # Extrae el texto de las columnas
+            text1 = page.within_bbox(left_bbox).extract_text()
+            text2 = page.within_bbox(right_bbox).extract_text()
+
+            
+            text_col1.append(text1.replace("-\n", "").replace("\n", " "))
+            text_col2.append(text2.replace("-\n", "").replace("\n", " "))
+        
+
+        text_1 = " ".join(text_col1)
+        text_2 = " ".join(text_col2)
+
+        if text_1 == "" or text_2 == "":
+            print("No se ha podido extraer texto de ninguna columna: ", fecha)
+        return text_1, text_2
+
+
+def main(fname_texts: str, num_legislature: str):
+    parser = etree.XMLParser(remove_blank_text=True) # discard whitespace nodes
+    tree_texts = etree.parse(fname_texts, parser)
+
+
+    print("ANALIZANDO FICHEROS...")
+
+    json_outputs = []
+    for i, art_elem in enumerate(tree_texts.xpath("//sesiones_pleno")):
+
+        legislatura = art_elem.xpath("./sesiones_pleno_legislatura")[0].text
+        id = art_elem.xpath("./sesiones_pleno_num_sesion")[0].text
+        fecha = art_elem.xpath("./sesiones_pleno_fecha_inicio")[0].text
+        pdf_path = art_elem.xpath("./sesiones_pleno_diario_link")[0].text
+
+        url = pdf_path.replace("<![CDATA[", "").replace("]]>", "").strip()
+
+        # descargar el PDF
+        download_pdf(url)
+        
+        # extraemos las columnas de texto
+        texts = extract_columns("doc_temp.pdf", fecha)
+        if texts is None:
+            continue
+
+        text1 = texts[0]
+        text2 = texts[1]
+
+        # guardar en json la info con las dos columnas
+        json_output = {
+                "legislatura": legislatura,
+                "num_sesion": id,
+                "fecha": fecha,
+                "original": text1,
+                "traduccion": text2,
+                "url": url,
+            }
+        json_outputs.append(json_output)
+
+    # Guardamos el JSON en un fichero
+    with open(f"json_output_{num_legislature}.json", "w") as f:
+        json.dump(json_outputs, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parse PDF and extract text columns")
+    parser.add_argument("--input_xml", type=str, help="Path to input XML file")
+    parser.add_argument("--num_legislature", type=str, help="Legislature number")
+    args = parser.parse_args()
+    main(args.input_xml, args.num_legislature)
